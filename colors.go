78736bd43b4d1c09bd8a5c12c6827c5d7449e3da
ysugimoto <sugimoto@wnotes.net>
2015-05-21 15:26:33 +0900

    added colors

---
+++ colors.go
@@ -0,0 +1,12 @@
+package gang
+
+import (
+	"github.com/mgutz/ansi"
+)
+
+var (
+	RED    = ansi.ColorCode("red")
+	REDB   = ansi.ColorCode("red+b")
+	GREEN  = ansi.ColorCode("green")
+	GREENB = ansi.ColorCode("green")
+)
